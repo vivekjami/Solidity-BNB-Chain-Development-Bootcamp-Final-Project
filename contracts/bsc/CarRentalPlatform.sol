fb9dabbcf158c549ec6cd39ccb257dcb8221cbb3
JAMI VIVEK <109971110+vivekjami@users.noreply.github.com>
2023-09-28 12:48:01 +0530

    first
    this is the basic outline of the carRentalPlatform in the bootcamp

---
+++ contracts/bsc/CarRentalPlatform.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract CarRentalPlatform {
+  //DATA
+
+  //Counter
+
+  //Owner
+
+  //totalPayments
+
+  //user struct
+
+  //car struct
+
+  //enum to indicate the status of the car
+
+  //events
+
+  //user mapping
+
+  //car mapping
+
+  //constructor
+
+  //MODIFIERS
+  //onlyOwner
+
+  //FUNCTIONS
+  //Execute FUNCTIONS
+
+  //setOwner #onlyOwner
+
+  //addUser #nonExisting
+
+  //addCar #onlyOwner #nonExistingCar
+
+  //editCarMetadata #onlyOwner #existingcar
+
+  //editCarStatus #onlyOwner #existingcar
+
+  //checkOut #existingUser #isCarAvailable #userHasNotRentedACar #userHasNoDebt
+
+  //checkIn #existingUser #userHasNotRentedACar
+
+  //deposit #existingUser
+
+  //makePayment #existingUser #existingDebt #sufficientBalance
+
+  //withdrawBalance #existingUser
+
+  //withdrawOwnerBalance #onlyOwner
+
+  //Query functions
+
+  //getOwner
+
+  //isUser
+
+  //getUser #existingUser
+
+  //getCar #existingcar
+
+  //getCarByStatus
+
+  //calculateDebt
+
+  //getCurrentCount
+
+  //getContractBalance #onlyOwner
+
+  //getTotalPayments #onlyOwner
+
+  
+}
