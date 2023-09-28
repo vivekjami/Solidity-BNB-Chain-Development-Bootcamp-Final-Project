77aff8cf6e18cdefc4efaa8202dd8dcf19abaf4f
JAMI VIVEK <109971110+vivekjami@users.noreply.github.com>
2023-09-28 13:22:31 +0530

    events
    listing events

--- contracts/bsc/CarRentalPlatform.sol
+++ contracts/bsc/CarRentalPlatform.sol
@@ -1,22 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 contract CarRentalPlatform {
   //DATA
 
   //Counter
+  using Counters for Counters.Counter;
+  Counters.Counter private_counter;
 
   //Owner
+  address private owner;
 
   //totalPayments
+  uint private totalPayments;
 
   //user struct
+  struct User{
+    address walletAddress;
+    string name;
+    string lastname;
+    uint rentedCarId;
+    uint balance;
+    uint debt;
+    uint start;
+  }
 
   //car struct
+  struct Car {
+    uint id;
+    string name;
+    string imgUrl;
+    Status status;
+    uint rentFee;
+    uint saleFee;
+  }
 
   //enum to indicate the status of the car
+  enum Status {
+    Retired,
+    InUse,
+    Available
+  }
 
   //events
+  event CarAdded(uint indexed id,string name,string imgUrl, uint rentFee, uint saleFee );
+  event CarMetadataEdited(uint indexed id, string name , string imgUrl, uint rentFee, uint saleFee);
+  event CarStatusEdited(uint indexed id, Status status);
+  event UserAdded(address indexed walletAddress,string name, string lastname);
+  event Deposit(address indexed walletAdderss, uint amount);
+  event CheckOut(address indexed walletAddress, uint carId );
+  event CheckIn(address indexed walletAddress, uint indexed carId );
+  event PaymentMade(address indexed walletAdderss, uint amount);
+  event BalanceWithdrawn(address indexed walletAddress, uint amount);
 
   //user mapping
 
