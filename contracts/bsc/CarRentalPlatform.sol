613a39283bfb445cd5f70c4f537593ff4f70d297
JAMI VIVEK <109971110+vivekjami@users.noreply.github.com>
2023-09-30 20:14:25 +0530

    changing truffle config

--- contracts/bsc/CarRentalPlatform.sol
+++ contracts/bsc/CarRentalPlatform.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
--- truffle-config.js
+++ truffle-config.js
@@ -55,21 +55,21 @@ module.exports = {  /**
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
+     timeout: 100000
   },
 
   // Configure your compilers
   compilers: {
     solc: {
       version: "0.8.7",       // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+      optimizer: {
+      enabled: false,
+      runs: 200
+       },
+       evmVersion: "byzantium"
+       }
     }
   },
 
